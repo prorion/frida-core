8f29b5772187961d6c659fd0852067a3ab768c16
prorion <prorion@naver.com>
2025-03-09 22:24:27 +0900

    Revert "이름변경"
    This reverts commit 1f81eca8995fc6271871bd82fdb736cc527010eb.

--- lib/agent/agent-glue.c
+++ lib/agent/agent-glue.c
@@ -72,7 +72,7 @@ JNI_OnLoad (JavaVM * vm, void * reserved)
 {
   FridaAgentBridgeState * state = reserved;
 
-  _myagnt_main (state->agent_parameters, &state->unload_policy, state->injector_state);
+  myagnt_main (state->agent_parameters, &state->unload_policy, state->injector_state);
 
   return JNI_VERSION_1_6;
 }
